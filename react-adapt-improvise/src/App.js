9f11c2c30a20dc2776ce61dd2d5b8f5d09c2734b
bradleytevans <bradleyt.evans@gmail.com>
2022-01-23 12:27:53 -0500

    setting up files

---
+++ react-adapt-improvise/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import AboutMe from './components/AboutMe';
+
+function App() {
+  const [categories] = useState([
+    {
+      name
+    }
+  ])
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+      <AboutMe></AboutMe>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-adapt-improvise/src/components/AboutMe/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+function AboutMe() {
+  return (
+    <section className="my-5">
+      <h1 id="aboutMe">About Me</h1>
+      <div className="my-2">
+        <p>
+          lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+          bibendum turpis sed ex condimentum molestie. Mauris condimentum lectus
+          ut ornare dignissim. Mauris faucibus urna mi, ac feugiat metus aliquam
+          maximus. Proin aliquam justo nec diam vulputate vestibulum. Aenean
+          sollicitudin nulla at nisi ornare, nec suscipit massa eleifend.
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default AboutMe;
+
