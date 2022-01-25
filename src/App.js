20e031211c992c2de8fffea89713c4f2d7dfc9ee
Bradleytevans <bradleyt.evans@gmail.com>
2022-01-24 22:45:13 -0500

    redid the routes and links in navbar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
 import React from "react";
 import './App.css';
-import AboutMe from './Pages/AboutMe';
-import ContactForm from './Pages/Contact';
 import Navibar from "./components/Navbar/Navbar";
-import Portfolio from "./Pages/Portfolio";
-import Resume from "./Pages/Resume";
 import Footer from "./components/Navbar/Footer"
-import { BrowserRouter, Routes ,Route } from 'react-router-dom';
-
 export default function App() {
   return (
     <div className="App">
         <Navibar></Navibar>
-    <BrowserRouter>
-        <Routes>
-        <Route path="/React-Adapt-Improvise" element={AboutMe()}/>
-        <Route path="/AboutMe" element={AboutMe()}/>
-        <Route path="/Projects" element={Portfolio()}/>
-        <Route path="/ContactForm" element={ContactForm()}/>
-        <Route path="/Resume" element={Resume()}/>
-        </Routes>
-    </BrowserRouter>
       <Footer></Footer>
     </div>
   );
