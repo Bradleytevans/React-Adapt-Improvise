4e357c89204476b9569117fa88e47a71e02d9e41
Bradleytevans <bradleyt.evans@gmail.com>
2022-01-24 22:06:41 -0500

    cleanup

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,27 +4,34 @@ import { Navbar, Container, Nav } from "react-bootstrap";
 
 export default function Navibar() {
   return (
-      <Navbar collapseOnSelect expand="sm" bg="dark" variant="dark pills" fixed="top" className="navb">
-        <Container>
-          <Navbar.Brand href="/">Bradley Evans</Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Item className="current">
-                <Nav.Link className="current" href="/AboutMe">About Me</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link className="current" href="/Projects">Projects</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link className="current" href="/ContactForm">Contact</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link className="current" href="/Resume">Resume</Nav.Link>
-              </Nav.Item>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+    <Navbar
+      collapseOnSelect
+      expand="sm"
+      bg="dark"
+      variant="dark pills"
+      fixed="top"
+      className="navb"
+    >
+      <Container>
+        <Navbar.Brand href="/">Bradley Evans</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Item>
+              <Nav.Link href="/AboutMe">About Me</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="/Projects">Projects</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="/ContactForm">Contact</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="/Resume">Resume</Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
